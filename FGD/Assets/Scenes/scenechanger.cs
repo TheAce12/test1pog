5b04f6b0b0f66a2f9b36705d51e8d05c64e01eb0
TheAce12 <80925584+TheAce12@users.noreply.github.com>
2021-12-28 23:46:58 +0300

    Script

--- FGD/Assets/Scenes/scenechanger.cs
+++ FGD/Assets/Scenes/scenechanger.cs
@@ -1,10 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.SceneManagement;
 
 namespace Scenes
 {
     public class scenechanger : MonoBehaviour
     {
-        
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 }
